2c1aa3d8e0155d080d140e73900336e7e999d0a7
Amanda Gálvez Corell <agalvezcorell@gmail.com>
2020-03-07 20:47:48 +0100

    Haciendo pruebas

--- flujo/funtexto.py
+++ flujo/funtexto.py
@@ -16,3 +16,29 @@ def arrayReshape(lista):
     array = np.asarray(lista)
     otro = array.reshape(300,300)
     return otro
+
+
+def formaUno(normalizado):
+
+    #Generamos 4 listas desde normalizado ordenadas de diferente manera.
+    no2 = sorted(normalizado)
+    no3 = sorted(normalizado,key = lambda a: a%2 == 0)
+    no4 = sorted(no3)
+    
+    #sumamos todas y las aplanamos
+    todas = [normalizado,no3,no3,no4]
+    normalizado2 = [a for b in todas for a in b]
+
+    #la convertimos en nparray para trabajar con ello
+    normalizado2 = np.asarray(normalizado2)
+    
+    #para multiplicar ese array lo convertimos momentáneamente en lista
+    normalizado2 = list(normalizado2) * 750
+    
+    return normalizado2
+
+def formaDos(normalizado):
+    normalizado2 = np.asarray(normalizado)
+    normalizado2 = np.sort(normalizado)
+    normalizado2 = list(normalizado) * 3000
+    return normalizado2
