ae95439176e40e2aa2b9d38ea3b2e31bc3c78ffb
Amanda Gálvez Corell <agalvezcorell@gmail.com>
2020-03-11 20:25:43 +0100

    Funcionando interfaz 🚀🔝

--- flujo/graba.py
+++ flujo/graba.py
@@ -1,12 +1,13 @@
 import speech_recognition as sr
+from tkinter import messagebox as mb
 r = sr.Recognizer()
 
 
 def recogeTexto():
     with sr.Microphone() as source:
-        print("Expresa algo")
+        mb.showinfo("Voy a grabar", "Expresa lo que quieras crear")
         audio1 = r.record(source, duration=13)
-        print("Grabación terminada. Procesando...")
+        mb.showinfo("Grabando", "Grabación terminada, voy a procesarla")
         texto = r.recognize_google(audio1, language='es_ES')        
         with open("audio.wav", "wb") as f:
             f.write(audio1.get_wav_data())
---
+++ guy.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from PIL import ImageTk
+import PIL.Image
+from flujo.ejecuciontkinter import *
+
+def main():
+    global window
+    HEIGHT = 600
+    WIDTH = 600
+    window = Tk()
+    window.title("WHEN NEURONAL NETS BECOMES EMOTIONS IN ART")
+    canvas = Canvas(window, height=HEIGHT, width=WIDTH)
+    canvas.pack()
+
+
+    # background
+    image = PIL.Image.open("sentimientos/fondo.jpg")
+    background_image = ImageTk.PhotoImage(image)
+    background_label = Label(window, image=background_image)
+    background_label.place(relwidth=1, relheight=1)
+
+
+    #Para el boton1
+    frame = Frame(window, bg='white', bd=5)
+    frame.place(relx=0.302, rely=0.16, relwidth=0.375,
+                relheight=0.1, anchor='n')
+    button = Button(frame,text="Empieza a crear",)
+    button.place(relx=0, relheight=1, relwidth=1)
+    button.configure(foreground='#ed7a9e', relief='groove',command= lambda: primeraParte())
+
+    #Para el boton
+    frame = Frame(window, bg='white', bd=5)
+    frame.place(relx=0.302, rely=0.25, relwidth=0.375,
+                relheight=0.1, anchor='n')
+    button = Button(frame,text="Segunda parte",)
+    button.place(relx=0, relheight=1, relwidth=1)
+    button.configure(foreground='#ed7a9e', relief='groove',command= lambda: segundaParte())
+
+
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+        main()
